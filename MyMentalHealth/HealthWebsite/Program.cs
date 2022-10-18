2c0f321421df80f4e545b5e5ced06e3f646aa4a1
SanaKadiwal <sanakad.cs@gmail.com>
2022-10-17 18:50:33 -0600

    fix: added user controller

--- MyMentalHealth/HealthWebsite/Program.cs
+++ MyMentalHealth/HealthWebsite/Program.cs
@@ -1,6 +1,8 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<UserContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("UserContext") ?? throw new InvalidOperationException("Connection string 'UserContext' not found.")));
 builder.Services.AddDbContext<AdminContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("AdminContext") ?? throw new InvalidOperationException("Connection string 'AdminContext' not found.")));
 
