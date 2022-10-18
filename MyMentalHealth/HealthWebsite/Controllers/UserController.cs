648d75b97fe8436817fd19325913337c79c6ef70
SanaKadiwal <sanakad.cs@gmail.com>
2022-10-17 18:51:04 -0600

    fix: added user controller

---
+++ MyMentalHealth/HealthWebsite/Controllers/UserController.cs
@@ -0,0 +1,162 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MyMentalHealth.Models;
+
+namespace MyMentalHealth.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+        }
+
+        // GET: User
+        public async Task<IActionResult> Index()
+        {
+              return _context.UserModel != null ? 
+                          View(await _context.UserModel.ToListAsync()) :
+                          Problem("Entity set 'UserContext.UserModel'  is null.");
+        }
+
+        // GET: User/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null || _context.UserModel == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.UserModel
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+
+        // GET: User/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: User/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("UserId,FirstName,LastName,DateOfBirth,UserName,Password")] UserModel userModel)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(userModel);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userModel);
+        }
+
+        // GET: User/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null || _context.UserModel == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.UserModel.FindAsync(id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+            return View(userModel);
+        }
+
+        // POST: User/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("UserId,FirstName,LastName,DateOfBirth,UserName,Password")] UserModel userModel)
+        {
+            if (id != userModel.UserId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(userModel);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!UserModelExists(userModel.UserId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(userModel);
+        }
+
+        // GET: User/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null || _context.UserModel == null)
+            {
+                return NotFound();
+            }
+
+            var userModel = await _context.UserModel
+                .FirstOrDefaultAsync(m => m.UserId == id);
+            if (userModel == null)
+            {
+                return NotFound();
+            }
+
+            return View(userModel);
+        }
+
+        // POST: User/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            if (_context.UserModel == null)
+            {
+                return Problem("Entity set 'UserContext.UserModel'  is null.");
+            }
+            var userModel = await _context.UserModel.FindAsync(id);
+            if (userModel != null)
+            {
+                _context.UserModel.Remove(userModel);
+            }
+            
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool UserModelExists(int id)
+        {
+          return (_context.UserModel?.Any(e => e.UserId == id)).GetValueOrDefault();
+        }
+    }
+}
---
+++ MyMentalHealth/HealthWebsite/Models/UserModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyMentalHealth.Models
+{
+    public class UserModel
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateOnly DateOfBirth { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+        public UserModel()
+        {
+        }
+    }
+}
+
