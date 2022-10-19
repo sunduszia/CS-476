296d1f19a67755fa94d52767614e81f0368c4f7e
uzbanasir <69500487+uzbanasir@users.noreply.github.com>
2022-10-19 10:23:40 -0600

    MariaDb connected!

---
+++ MyMentalHealth/HealthWebsite/Data/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace MyMentalHealth.Data
+{
+    public partial class User
+    {
+        public int UserId { get; set; }
+        public string UName { get; set; }
+        public byte[] UPassword { get; set; }
+        public string UEmail { get; set; }
+    }
+}
