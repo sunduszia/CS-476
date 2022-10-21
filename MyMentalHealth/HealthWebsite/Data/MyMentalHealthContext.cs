d3f28f15298a909a82a652f8b8649ff1082f05ca
uzbanasir <69500487+uzbanasir@users.noreply.github.com>
2022-10-21 14:37:25 -0600

    connection string

--- MyMentalHealth/HealthWebsite/Data/MyMentalHealthContext.cs
+++ MyMentalHealth/HealthWebsite/Data/MyMentalHealthContext.cs
@@ -25,7 +25,7 @@ namespace MyMentalHealth.Data
             if (!optionsBuilder.IsConfigured)
             {
 //#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseMySQL("server=Uzbas-MacBook-Air.local; database=MyMentalHealth; Trusted_Connection=true;");
+                optionsBuilder.UseMySQL("server=localhost; User=root; Password=my-secret-pw; Database=MyMentalHealth;");
             }
         }
 
