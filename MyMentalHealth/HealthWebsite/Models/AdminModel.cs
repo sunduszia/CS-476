212ef054996628056da9eccaf99bfdec04c4b70c
SanaKadiwal <sanakad.cs@gmail.com>
2022-10-29 13:18:06 -0600

    fix: added admin login

--- MyMentalHealth/HealthWebsite/Models/AdminModel.cs
+++ MyMentalHealth/HealthWebsite/Models/AdminModel.cs
@@ -11,11 +11,23 @@ namespace MyMentalHealth.Models
         public int Id { get; set; }
 
         [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [EmailAddress]
         public string UserName { get; set; }
 
         [Required]
+        [StringLength(100, MinimumLength = 2)]
+        [DataType(DataType.Password)]
         public string Password { get; set; }
-   
+
+        [Display(Name = "Remember Me")]
+        public bool RememberMe { get; set; }
+
+        public string LoginInValid { get; set; }
+
+        public string LoginFailedMessage { get; set; }
+
+
     }
 }
 
