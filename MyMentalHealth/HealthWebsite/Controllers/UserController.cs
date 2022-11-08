ee7e4ed6c75695cb9e17c6e01614adb3a554824f
SanaKadiwal <sanakad.cs@gmail.com>
2022-11-08 16:37:25 -0600

    fix: Added UserController

---
+++ MyMentalHealth/HealthWebsite/Controllers/UserController.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyMentalHealth.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+        public IActionResult Logout()
+        {
+            return View();
+        }
+    }
+}
